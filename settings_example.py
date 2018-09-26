15a71e8151a255f6c7ac2606bdd8eb802294e173
Avi Solomon <asolomon@dovelewis.org>
2018-09-26 14:37:16 -0700

    add example settings file

---
+++ settings_example.py
@@ -0,0 +1,8 @@
+SETTINGS {
+    "PROD_URL":"https://api.ezyvet.com/",
+    "SAND_URL":"https://api.trial.ezyvet.com/v1/",
+    "SB_LOGIN":"",
+    "SB_PASS":"",
+    "CLIENT_ID":"",
+    "CLIENT_SECRET":""
+}
