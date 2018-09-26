6f085c8ccb29920eb6a79923a9a325f8304a1ac9
Avi Solomon <asolomon@dovelewis.org>
2018-09-26 16:19:46 -0700

    add PARTNER to settings example

--- settings_example.py
+++ settings_example.py
@@ -1,6 +1,7 @@
 SETTINGS {
     "PROD_URL":"https://api.ezyvet.com/",
     "SAND_URL":"https://api.trial.ezyvet.com/v1/",
+    "PARTNER_ID":"",
     "SB_LOGIN":"",
     "SB_PASS":"",
     "CLIENT_ID":"",
