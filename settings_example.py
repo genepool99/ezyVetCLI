800a9ad2e57c388f0d9e880477593a70a6d34b8d
Avi Solomon <asolomon@dovelewis.org>
2018-10-24 14:55:33 -0700

    update example settings

--- settings_example.py
+++ settings_example.py
@@ -1,9 +1,13 @@
 SETTINGS = {
-    "PROD_URL":"https://api.ezyvet.com/",
-    "SAND_URL":"https://api.trial.ezyvet.com/v1/",
-    "PARTNER_ID":"",
-    "SB_LOGIN":"",
+    "PROD_URL":"https://api.ezyvet.com/v1",
+    "SAND_URL":"https://api.trial.ezyvet.com/v1",
+    "USE_SAND":False,
+    "DEBUG":"debug",                                         # debug, info, warn, error
+    "SB_LOGIN":"",                                           # Sandbox login username
     "SB_PASS":"",
+    "PARTNER_ID":"",
+    "CLIENT_ID_SAND":"",
+    "CLIENT_SECRET_SAND":"",
     "CLIENT_ID":"",
     "CLIENT_SECRET":""
 }
